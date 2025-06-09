dd3212ce8bc3454b7ffd9d7a76bb1ceb0d33df00
pog205 <phongdtph48650@gmail.com>
2025-06-09 09:48:45 +0700

    giao diện detail thêm giỏ hàng

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
-<script setup></script>
+<script setup>
+import Cart from './view/detail/Cart.vue';
+import DetailProduct from './view/detail/detailProduct.vue';
+</script>
 
 <template>
   <h1>BookStation</h1>
+  <DetailProduct />
+  <Cart />
 </template>
 
-<style scoped></style>
+<style scoped>
+body {
+    background: #bdacac !important;
+}
+</style>
---
+++ src/view/detail/Cart.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h3>
+      Giỏ hàng
+      <h5>
+        <small class="cart-count">(1 sản phẩm)</small>
+      </h5>
+    </h3>
+  </div>
+</template>
+
+<script></script>
+
+<style>
+.cart-count {
+  font-size: 0.85em;
+}
+
+small {
+  font-size: 0.85em;
+}
+</style>
