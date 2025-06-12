269d2a60b6e97b4b1625b2080599e37bd9186f39
lilbugsecret <lamhuyp05@gmail.com>
2025-06-12 15:54:53 +0700

    stast

--- src/views/admin/views-admin/User.vue
+++ src/views/admin/views-admin/User.vue
@@ -1,39 +1,7 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="users"
-    class="elevation-1"
-    :items-per-page="5"
-  >
-    <template v-slot:item.actions="{ item }">
-      <v-btn color="primary" @click="editUser(item)">Edit</v-btn>
-      <v-btn color="error" @click="deleteUser(item.id)">Delete</v-btn>
-    </template>
-
-    <template v-slot:no-data>
-      <v-alert type="info">No users found.</v-alert>
-    </template>
-  </v-data-table>
+  <div class="btn btn-success">OK</div>
 </template>
 
 <script setup>
 import { ref } from "vue";
-
-const headers = [
-  { text: "ID", value: "id" },
-  { text: "Name", value: "name" },
-  { text: "Email", value: "email" },
-  { text: "Role", value: "role" },
-  { text: "Actions", value: "actions", sortable: false },
-];
-
-const users = ref([]);
-
-function editUser(user) {
-  console.log("Edit", user);
-}
-
-function deleteUser(id) {
-  console.log("Delete", id);
-}
 </script>
